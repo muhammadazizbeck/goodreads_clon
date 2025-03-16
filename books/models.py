f8313ac5f93c81e116d1a0cf028c092a00d75f12
A1909bek <aa2004bek@gmail.com>
2025-03-16 12:55:00 +0500

    initial commit

---
+++ books/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    author = models.ForeignKey(Author,on_delete=models.Case)
+
+class BookReview(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    comment = models.TextField()
+    stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    
+
+
